18e1d49082d01c44de8515451ff4e8cd64256266
Rhian McClelland <rhianmcclelland193@hotmail.com>
2024-04-21 10:00:46 +1000

    Initial structure setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# K(rusty)-Means
+K-means clustering algorithm implemented in Rust.
+Completed as an educational project.
+
+## Dataset
+Data is obtained from (here)[https://cs.joensuu.fi/sipu/datasets/]
---
+++ src/data/dataset.rs
@@ -0,0 +1,13 @@
+pub struct Dataset {
+    
+}
+
+impl Dataset {
+    pub fn new() -> Dataset {
+        Dataset {}
+    }
+
+    pub fn from_csv(file_path: &str) -> Dataset {
+        Dataset {}
+    }
+}
